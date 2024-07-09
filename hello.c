cc84c7db54a6add2f16697ebe6e7774a904d6d31
Bernardo Andrade <berdfandrade@gmail.com>
2024-07-09 15:08:28 -0300

    --

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void printHello(){
+	int i = 5; 
+	for(int j = 0; j < i; j++){
+		printf("Benny rules\n"); 
+	}
+}
+
+int main () {
+	printf("Hello World!\n");
+	printf("--------------------\n");
+	printHello(); 
+	return 0;
+} 
+
+
+
